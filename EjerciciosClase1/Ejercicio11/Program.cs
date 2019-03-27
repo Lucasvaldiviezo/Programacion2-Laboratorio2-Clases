b79f77b59cd3629cf99794703ece7f761dd16398
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-03-27 19:56:20 -0300

    Ejercicio 11 listo

---
+++ EjerciciosClase1/Ejercicio11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 10";
+            string auxValor;
+            int valor;
+            int minimo = -100;
+            int maximo = 100;
+            int minimoActual = int.MaxValue;
+            int maximoActual = int.MinValue;
+            float total=0;
+            float promedio;
+            bool validar;
+            int i;
+            int cantNumeroEntero = 10;
+            for(i=0;i<cantNumeroEntero;i++)
+            {
+                Console.Write("Ingrese el valor {0}: ",i+1);
+                auxValor = Console.ReadLine();
+                validar = int.TryParse(auxValor, out valor);
+                if (validar)
+                {
+                    validar = Validacion.Validar(valor, minimo, maximo);
+                    if(validar)
+                    {
+                        total = total + valor;
+                        if(valor > maximoActual)
+                        {
+                            maximoActual = valor;
+                        }
+                        if (valor < minimoActual)
+                        {
+                            minimoActual = valor;
+                        }
+                    }
+                }
+            }
+            promedio = total / cantNumeroEntero;
+            Console.Clear();
+            Console.WriteLine("El promedio es: {0}",promedio);
+            Console.WriteLine("El valor minimo es: {0}",minimoActual);
+            Console.WriteLine("El valor maximo es: {0}",maximoActual);
+
+            Console.ReadKey();
+        }
+    }
+}

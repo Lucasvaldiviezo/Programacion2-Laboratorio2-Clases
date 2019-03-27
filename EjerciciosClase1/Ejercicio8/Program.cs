b239e38556bc7efd3f7dda66e860b7fbf2403f10
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-03-27 19:11:13 -0300

    ejercicio 8 y 9 listos

---
+++ EjerciciosClase1/Ejercicio8/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 8";
+            int hora;
+            string nombre;
+            string auxHora;
+            string auxAntiguedad;
+            string auxCantHoras;
+            int antiguedad;
+            int cantHoras;
+            int seguir = 1;
+            string auxSeguir;
+            bool validar;
+            bool validar2;
+            bool validar3;
+            float total;
+            float descuento;
+
+            do
+            {
+                Console.Write("Ingrese Nombre del empleado: ");
+                nombre = Console.ReadLine();
+                Console.Write("Ingrese la antigüedad del empleado: ");
+                auxAntiguedad = Console.ReadLine();
+                Console.Write("Ingrese la cantidad de horas trabajadas del empleado: ");
+                auxCantHoras = Console.ReadLine();
+                Console.Write("Ingrese lo que cobra por hora el empleado: ");
+                auxHora = Console.ReadLine();
+                validar = int.TryParse(auxHora, out hora);
+                validar2 = int.TryParse(auxAntiguedad, out antiguedad);
+                validar3 = int.TryParse(auxCantHoras, out cantHoras);
+                if(validar && validar2 && validar3)
+                {
+                    total = hora * cantHoras;
+                    total = total + antiguedad * 150;
+                    descuento = total * 13 / 100;
+                    total = total - descuento;
+                    Console.WriteLine("El sueldo final del empleado es: {0}",total);
+                    Console.WriteLine("¿Desea ingresar otro empleado? 1=Si / 0 = No:");
+                    auxSeguir = Console.ReadLine();
+                    int.TryParse(auxSeguir, out seguir);
+                    if(seguir==0)
+                    {
+                        Console.WriteLine("¡Hasta pronto!");
+                    }else
+                    {
+                        Console.Clear();
+                    }
+                }
+            } while (seguir == 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjerciciosClase1/Ejercicio9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 9";
+            int cantLineas;
+            string auxCantLineas;
+            bool validar;
+            int i;
+            int j = 0;
+            Console.Write("Ingrese la cantidad de linea que tendra la piramide: ");
+            auxCantLineas = Console.ReadLine();
+            validar = int.TryParse(auxCantLineas, out cantLineas);
+            if(validar)
+            {
+                for (i = 1; i <= cantLineas; i++)
+                {
+                    do
+                    {
+                        Console.Write("*");
+                        j++;
+                    } while (j < i);
+                    j = 0;
+                    Console.WriteLine(" ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

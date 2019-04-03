ee0f2e221e8586deb3f03a40217f851fe8d3b6fc
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-03 20:04:50 -0300

    Ejercicio5Campus

---
+++ Ejercicio 5 Campus/Repaso/EjercicioCampus/Producto.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCampus
+{
+    class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca,string codigo,float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            string marca;
+            marca = this.marca;
+            return marca;
+        }
+
+        public float GetPrecio()
+        {
+            float precio;
+            precio = this.precio;
+            return precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            string cadena;
+            cadena = "La marca es " + p.marca + "\nEl codigo de barra es " + p.codigoDeBarra + "\nEl precio es " + p.precio;
+            return cadena;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+            if(p1.marca == p2.marca && 
+               p1.codigoDeBarra == p2.codigoDeBarra)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+            if (p1.marca != p2.marca ||
+               p1.codigoDeBarra != p2.codigoDeBarra)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool retorno = false;
+            if (p.marca == marca )
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            bool retorno = false;
+            if (p.marca != marca)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Ejercicio 5 Campus/Repaso/EjercicioCampus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCampus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "EjercicioCampus";
+            string codigoDeBarras;
+            Producto producto1 = new Producto("Pepitos","123",30);
+            codigoDeBarras = (string)producto1;
+            string marca = "Pepitos";
+            string auxMarca;
+            bool check;
+
+
+            check = (producto1 == marca);
+
+            Console.WriteLine("El codigo de barra es: {0}", codigoDeBarras);
+            Console.ReadKey();
+        }
+    }
+}

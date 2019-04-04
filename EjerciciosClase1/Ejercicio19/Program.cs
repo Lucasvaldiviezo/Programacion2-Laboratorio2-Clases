46bf80867038cb5c2f02954987a586e2f022c713
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-04 20:59:43 -0300

    Ejercicio 19 listo

---
+++ EjerciciosClase1/Ejercicio19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 19";
+            Sumador suma = new Sumador(4);
+            Sumador suma2 = new Sumador(5);
+            long numero1 = 2;
+            long numero2 = 3;
+            long resultado;
+            string mensaje1 = "Hola";
+            string mensaje2 = " y Chau";
+            string cadena;
+            int totalSumas;
+            long totalCantSumas;
+            bool cantSumasIguales;
+            resultado = suma.Sumar(numero1, numero2);
+            cadena = suma.Sumar(mensaje1, mensaje2);
+            totalSumas = (int)suma;
+            totalCantSumas = suma + suma2;
+            cantSumasIguales = suma | suma2;
+            Console.WriteLine("El resultado de la suma es: {0}", resultado);
+            Console.WriteLine("El mensaje es: {0}", cadena);
+            Console.WriteLine("El total de sumas es: {0}", totalSumas);
+            Console.WriteLine("El total de la cantidad de sumas es: {0}", totalCantSumas);
+            if(cantSumasIguales)
+            {
+                Console.WriteLine("El cantidad de sumas son iguales");
+            }else
+            {
+                Console.WriteLine("El cantidad de sumas no son iguales");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjerciciosClase1/Ejercicio19/Sumador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantSumas)
+        {
+            this.cantidadSumas = cantSumas;
+        }
+        public Sumador() : this(0){ }
+
+        public long Sumar(long numero1, long numero2)
+        {
+            long numeroRetorno;
+
+            this.cantidadSumas++;
+            numeroRetorno = numero1 + numero2;
+
+            return numeroRetorno;
+        }
+
+        public string Sumar(string cadena1, string cadena2)
+        {
+            string cadenaRetorno;
+
+            this.cantidadSumas++;
+            cadenaRetorno =  cadena1 + cadena2;
+
+            return cadenaRetorno;
+        }
+
+        public static explicit operator int(Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            long resultado;
+            resultado = s1.cantidadSumas + s2.cantidadSumas;
+            return resultado;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            bool resultado=false;
+            if (s1.cantidadSumas == s2.cantidadSumas)
+                resultado = true;
+            return resultado;
+        }
+
+
+    }
+}

a72dd71a2dc9846cb3419edb1585a50c5d3c8b9b
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-04 22:10:22 -0300

    No se

---
+++ ProbandoQueOnda/ProbandoQueOnda/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProbandoQueOnda
+{
+    partial class Probando
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.Ok = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblNombre.Location = new System.Drawing.Point(220, 9);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(94, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Ingrese su nombre";
+            this.lblNombre.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Ok
+            // 
+            this.Ok.Location = new System.Drawing.Point(229, 91);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 1;
+            this.Ok.Text = "OK";
+            this.Ok.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Probando
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 126);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Ok);
+            this.Controls.Add(this.lblNombre);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Name = "Probando";
+            this.Text = "Probando";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+

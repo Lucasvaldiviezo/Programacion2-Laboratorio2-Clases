abb57a4920eb32591a31d90992c85cf61efb3efa
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-11 22:07:44 -0300

    ejercicio 27 empezado

---
+++ EjerciciosClase1/Ejercicio27/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Ejercicio 27";
+            List<double> enteros = new List<double>(); ;
+            int cantidad = 20;
+            int i;
+            Random r = new Random();
+            for (i = 0; i < cantidad; i++)
+            {
+                double numero = r.Next(-100, 100);
+                if (numero != 0)
+                {
+                    enteros.Add(numero);
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            foreach (double entero in enteros)
+            {
+                Console.WriteLine("Numero: {0}", entero);
+            }
+
+            enteros.Sort(Program.OrdenarDescendente);
+
+            Console.WriteLine("-----------Lista Ordenada Positivos-----------");
+            foreach (double entero in enteros)
+            {
+                if (entero > 0)
+                {
+                    Console.WriteLine("Numero: {0}", entero);
+                }
+
+            }
+
+            enteros.Sort();
+
+            Console.WriteLine("-----------Lista Ordenada Negativos-----------");
+            foreach (double entero in enteros)
+            {
+                if (entero < 0)
+                {
+                    Console.WriteLine("Numero: {0}", entero);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int OrdenarDescendente(double numero1, double numero2)
+        {
+            return -numero1.CompareTo(numero2);
+        }
+    }
+}

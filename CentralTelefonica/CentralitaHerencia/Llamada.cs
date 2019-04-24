49975f9f514a1559999df1df16c8e820700c0b38
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-24 19:01:30 -0300

    Ejercicio 37 comenzado

---
+++ CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public enum TipoLlamada
+    {
+        Local,
+        Provincial,
+        Todas
+    }
+    public class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+        public float Duracion
+        {
+            get
+            {
+                return duracion;
+            }  
+        }
+        public string NroDestino
+        {
+            get
+            {
+                return nroDestino;
+            }
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return nroOrigen;
+            }
+        }
+
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendLine("Numero Origen: ");
+            mostrar.AppendLine(nroOrigen);
+            mostrar.AppendLine("/ Numero Destino: ");
+            mostrar.AppendLine(nroDestino);
+
+            return mostrar.ToString();
+        }
+
+        public int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            int retorno = 0;
+            if(llamada1.duracion > llamada2.duracion)
+            {
+                retorno = 1;
+            }else if(llamada1.duracion < llamada2.duracion)
+            {
+                retorno = -1;
+            }
+
+            return retorno;
+        }
+    }
+
+}

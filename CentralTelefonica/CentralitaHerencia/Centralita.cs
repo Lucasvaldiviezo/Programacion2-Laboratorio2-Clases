dd5caca836346ed23369459103d231d1196fdc60
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-24 20:06:27 -0300

    Avance jiji

--- CentralTelefonica/CentralitaHerencia/Centralita.cs
+++ CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -8,5 +8,7 @@ namespace CentralitaHerencia
 {
     public class Centralita
     {
+        private List<Llamada> listaDeLLamada;
+        protected string razonSocial;
     }
 }
---
+++ CentralTelefonica/CentralitaHerencia/Local.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Local : Llamada
+    {
+        protected float costo;
+        public Local(Llamada llamada, float costo) : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.costo = costo;
+        }
+        public Local(string origen,float duracion, string destino, float costo) : base(duracion, destino, origen)
+        {
+            this.costo = costo;
+        }
+    }
+}

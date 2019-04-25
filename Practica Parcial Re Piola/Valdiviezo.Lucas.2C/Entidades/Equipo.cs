c393f7a554a99a5ab439b4d379dac951c8877985
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-25 20:24:30 -0300

    TP casi

--- Practica Parcial Re Piola/Valdiviezo.Lucas.2C/Entidades/Equipo.cs
+++ Practica Parcial Re Piola/Valdiviezo.Lucas.2C/Entidades/Equipo.cs
@@ -36,7 +36,7 @@ namespace Entidades
         {
             StringBuilder mostrar = new StringBuilder();
 
-            mostrar.AppendFormat("|||Equipo: {0}|||", e.Nombre);
+            mostrar.AppendFormat("|||Equipo: {0}|||\nCantidad de Jugadores: {1}", e.Nombre,cantidadMaximaJugadores);
             if(e.directorTecnico != null)
             {
                 mostrar.AppendFormat("\n{0}", e.directorTecnico.Mostrar());
@@ -50,6 +50,46 @@ namespace Entidades
             }
 
             return mostrar.ToString();
+        }
+
+        public static bool operator ==(Equipo e,Jugador j)
+        {
+            bool retorno = false;
+            foreach(Jugador jugador in e.jugadores)
+            {
+                if(jugador == j)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Equipo e, Jugador j)
+        {
+            bool retorno = true;
+            foreach (Jugador jugador in e.jugadores)
+            {
+                if (jugador == j)
+                {
+                    retorno = false;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        public static Equipo operator +(Equipo e, Jugador j)
+        {
+           
+            if(e.jugadores.Count < 6 && e != j && j.ValidarAptitud())
+            {
+                e.jugadores.Add(j);
+            }
+
+            return e;
+            
         }
     }
 }

543609a005892244181a35625661df6dcac9c9ed
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-05-02 20:17:53 -0300

    Pratica Parcial 2

---
+++ Practica Parcial Re Piola 2/Valdiviezo.Lucas.2C/Entidades/Automovil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        public override bool Equals(object obj)
+        {
+            return obj is Automovil;
+        }
+
+        private ConsoleColor color;
+        private static int valorHora;
+
+        static Automovil()
+        {
+            valorHora = 50;
+        }
+
+        public Automovil(string patente,ConsoleColor color) : base(patente)
+        {
+            this.color = color;
+        }
+
+        public Automovil(string patente, ConsoleColor color, int valorHora) : base(patente)
+        {
+            this.color = color;
+            Automovil.valorHora = valorHora;
+        }
+
+        public override string ConsultarDatos()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendLine("||Automovil||");
+            mostrar.AppendFormat("{0}\n Color: {1}\n Valor de Hora: {2}",base.ImprimirTicket(),color,valorHora);
+
+        }
+    }
+}
---
+++ Practica Parcial Re Piola 2/Valdiviezo.Lucas.2C/Entidades/Vehiculo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Vehiculo
+    {
+        protected DateTime ingreso;
+        private string patente;
+
+        public string Patente
+        {
+            set {
+                    if(value.Length == 6)
+                    {
+                        patente = value;
+                    }
+                }
+            get { return patente; }
+
+        }
+
+        public Vehiculo(string patente)
+        {
+            Patente = patente;
+            ingreso = DateTime.Now.AddHours(-3);
+        }
+
+        public abstract string ConsultarDatos();
+
+        public override string ToString()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendFormat(" Patente {0}", Patente);
+
+            return mostrar.ToString();
+        }
+
+        public virtual string ImprimirTicket()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendLine("------Vehiculo------");
+            mostrar.AppendFormat(" Ingreso {0}", ingreso);
+            mostrar.AppendLine(this.ToString());
+
+            return mostrar.ToString();
+        }
+
+        public static bool operator ==(Vehiculo v1,Vehiculo v2)
+        {
+            bool retorno = false;
+            if(v1.Equals(v2) && v1.Patente == v2.Patente)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}

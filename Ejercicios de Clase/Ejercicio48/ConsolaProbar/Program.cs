88d59af513336e665d3d28a5ba16a8ecb18f2064
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-05-23 20:43:41 -0300

    Ejercicio 48 list

---
+++ Ejercicios de Clase/Ejercicio48/ConsolaProbar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace ConsolaProbar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contabilidad<Factura, Recibo> contable = new Contabilidad<Factura, Recibo>();
+            Factura factura1 = new Factura(20);
+            Factura factura2 = new Factura(30);
+            Factura factura3 = new Factura(40);
+            Recibo recibo1 = new Recibo(50);
+            Recibo recibo2 = new Recibo();
+            Recibo recibo3 = new Recibo(70);
+
+            contable += factura1;
+            contable += factura2;
+            contable += factura3;
+            contable += recibo1;
+            contable += recibo2;
+            contable += recibo3;
+
+
+            Console.WriteLine(contable.Mostrar());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios de Clase/Ejercicio48/Entidades/Contabilidad.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Contabilidad<T,U> where T : Documento 
+                                   where U : Documento, new()
+    {
+        private List<T> egresos;
+        private List<U> ingresos;
+
+        public Contabilidad()
+        {
+            egresos = new List<T>(); 
+            ingresos = new List<U>();
+        }
+
+        public static Contabilidad<T,U> operator +(Contabilidad<T,U> c, T egreso)
+        {
+            c.egresos.Add(egreso);
+            return c;
+        }
+
+        public static Contabilidad<T, U> operator +(Contabilidad<T, U> c, U ingreso)
+        {
+            c.ingresos.Add(ingreso);
+            return c;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendLine("Lista de Egresos:");
+            foreach(Documento d in egresos)
+            {
+                mostrar.AppendFormat("-{0}\n", d.NumeroBase);
+            }
+            mostrar.AppendLine("-----------------------");
+            mostrar.AppendLine("Lista de Ingresos:");
+            foreach(Documento d in ingresos)
+            {
+                mostrar.AppendFormat("-{0}\n", d.NumeroBase);
+            }
+
+            return mostrar.ToString();
+        }
+    }
+}

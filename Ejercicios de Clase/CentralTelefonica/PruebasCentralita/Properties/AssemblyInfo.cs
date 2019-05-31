18ad3ed59bd9098ca60fd1c0142069abb0640cb8
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-05-30 22:16:08 -0300

    Empezando centralita test unitarios

---
+++ Ejercicios de Clase/CentralTelefonica/PruebasCentralita/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasCentralita")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PruebasCentralita")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("800f678d-3789-4474-aa2a-1fd85c883f4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Ejercicios de Clase/CentralTelefonica/PruebasCentralita/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentralitaHerencia;
+
+namespace PruebasCentralita
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstancedList()
+        {
+            //arrange
+            Centralita nuevaCentralita = new Centralita();
+            //act
+            //assert
+            Assert.IsNotNull(nuevaCentralita.Llamadas);
+        }
+    }
+}
